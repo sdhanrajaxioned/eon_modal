ff43c1be904b47fa34a14a9a108b1878554297ce
dhanraj shetty <dhanrajshetty1997@gmail.com>
2022-03-30 15:23:14 +0530

    fixed modal functionality

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -6,29 +6,29 @@ $(document).ready(function () {
         $('body').toggleClass('hidden')
     })
 
-    function video_toggle() {
-        var video_container = document.querySelector('.video-container')
-        video_container.classList.toggle('active');
-        var video = document.querySelector('video');
+    $('.modal-btn').click(function (e) {
+        var video_container = $(this).prev();
+        video_container.addClass('active')
+        var video = $(this).prev().find('video').get(0);
+        if (video_container.hasClass('active')) {
+            video.play()
+            video.currentTime = 0;
+            $(video_container).click(function (e) {
+                if (e.target === video_container.get(0)) {
+                    video_container.removeClass('active')
+                    video.pause();
+                    video.currentTime = 0;
+                }
+            });
+        }
+    })
+
+    $('.modal-close').click(function () {
+        var video_container = $(this).parent();
+        video_container.removeClass('active');
+        var video = $(this).prev().get(0);
         video.pause();
         video.currentTime = 0;
-    }
+    })
 
-    $('.modal-btn').click(video_toggle);
-    $('.modal-close').click(video_toggle);
-
-    // $('.modal-btn').click(function () {
-    //     $('.video-container').addClass('active');
-    //     var video = $(this).prev().find('video');
-    //     // console.log(video)
-    //     video.trigger('pause');
-    //     video.currentTime = 0;
-    // })
-    // $('.modal-close').click(function () {
-    //     $('.video-container').removeClass('active');
-    //     var video = $(this).prev().find('video');
-    //     // console.log(video)
-    //     video.trigger('pause');
-    //     video.currentTime = 0;
-    // })
 });
