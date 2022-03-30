b6a851efb99924d73c330c880948b6ff7ffcc12d
dhanraj shetty <dhanrajshetty1997@gmail.com>
2022-03-30 16:47:34 +0530

    added drop down menu in hamburger

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -3,7 +3,36 @@ $(document).ready(function () {
     $('.hamburger').click(function () {
         $(this).toggleClass('active')
         $('nav').toggleClass('active');
-        $('body').toggleClass('hidden')
+        $('body').toggleClass('hidden');
+
+        $('.topics').click(function () {
+            $('.topics-dropdown').toggleClass('active');
+            if ($('.topics-dropdown').hasClass('active')) {
+                $('.topics-dropdown').parentsUntil('.wrapper-header').find('.nav-right').hide().parent().find('.topic-menu').siblings().hide();
+            } else {
+                $('.topics-dropdown').parentsUntil('.wrapper-header').find('.nav-right').show().parent().find('.topic-menu').siblings().show();
+            }
+        })
+
+        $('.more').click(function () {
+            $('.more-dropdown').toggleClass('active');
+            if ($('.more-dropdown').hasClass('active')) {
+                $('.more-dropdown').parentsUntil('.wrapper-header').find('.nav-right').hide().parent().find('.more-menu').siblings().hide();
+            }
+            else {
+                $('.more-dropdown').parentsUntil('.wrapper-header').find('.nav-right').show().parent().find('.more-menu').siblings().show();
+
+            }
+        })
+
+        $('.register').click(function () {
+            $('.manage-subscription').toggleClass('active');
+            if ($('.manage-subscription').hasClass('active')) {
+                $('.manage-subscription').parentsUntil('.wrapper-header').find('.nav-left').hide().parent().find('.subscription').siblings().hide();
+            } else {
+                $('.manage-subscription').parentsUntil('.wrapper-header').find('.nav-left').show().parent().find('.subscription').siblings().show();
+            }
+        })
     })
 
     $('.modal-btn').click(function (e) {
@@ -30,5 +59,7 @@ $(document).ready(function () {
         video.pause();
         video.currentTime = 0;
     })
+
+
 
 });
